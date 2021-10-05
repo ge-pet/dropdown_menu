2a3d289f704ef9a9f6fb4481c94858d2b71ad4ed
Ge Pet <ge.petavvrakis@gmail.com>
2021-10-05 18:28:38 +0300

    Styling and more

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-var drpl = document.querySelector(".dropdown-list");
-var tclass = document.querySelector(".testclass");
+// var drpl = document.getElementsByClassName("dropdown-list");
+// var test = document.getElementById("test").style;
+// console.log(test);
 
-var styling = drpl.style.display;
+// var t = document.querySelector(".dropdown-list");
 
-tclass.addEventListener("click", () => {
+// var tclass = document.querySelector(".testclass");
+
+// var styling = drpl.style.display;
+
+// tclass.addEventListener("click", () => {
+//   if (styling === "none") {
+//     styling = "block";
+//     //    console.log(styling);
+//   }
+//   //  console.log(styling);
+// });
+
+var click = document.getElementById("test");
+
+var dropdown = document.querySelector("dropdown-list");
+
+// var styling = dropdown.
+
+click.addEventListener("click", () => {
+  /* 
   if (styling === "none") {
-    styling = "block";
-    console.log(styling);
-  }
-  console.log(styling);
+     styling = "block"
+  } else {styling="none"}
+  */
+
+  console.log("clicked!!!");
 });
